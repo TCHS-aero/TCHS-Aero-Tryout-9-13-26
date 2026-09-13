9153c5612f15a09f6bbf24237cbf55ba0146685a
1Victorzou <zouvictor3@gmail.com>
2026-09-13 18:28:36 +0000

    Victor's tryout

--- ReverseEngineer.py
+++ ReverseEngineer.py
@@ -1,62 +1,65 @@
-import asyncio
+import asyncio#support asyncio operations
 from dataclass import dataclass
-from typing import NamedTuple
-from mavsdk import System
+from typing import NamedTuple#from typing import NamedTuple
+from mavsdk import System# From mavsdk import system
 from mavsdk.offboard import PositionGlobalYaw, VelocityNedYaw
 
 @dataclass
-class NedPosition(NamedTuple):
+class NedPosition(NamedTuple):#store the dron's postion
     north: float
     east: float
     down: float
 
-class Drone(asyncio):
-    def __init__(self, port = "udpin://0.0.0.0:14540"):
-        self.drone = System()
-        self.port = "udpin://0.0.0.0:14540"
+class Drone(asyncio):#control the drone
 
-    async def connect(self):
+
+
+    def __init__(self, port = "udpin://0.0.0.0:14540"):#sset up the drone.
+        self.drone = System()#create the drone object
+        self.port = "udpin://0.0.0.0:14540"#save the given addresss.
+
+    async def connect(self):#wait for the drone to connect
         connected = False
 
-        await self.drone.connect(system_address=self.port)
+        await self.drone.connect(system_address=self.port)#use this address.
         async for state in self.drone.core.connection_state():
-            if state.is_connected:
+            if state.is_connected:#check that if the drone is connected
                 break
 
-        return connected
+        return connected# return the connectionn result
 
-    def takeoff(self, alt)
-        async for health_check in self.drone.telemetry.health():
-            if health_check.is_global_position_ok and health_check.is_home_position_ok:
+    def takeoff(self, alt)#take the target height
+        async for health_check in self.drone.telemetry.health():#read the health updates
+            if health_check.is_global_position_ok and health_check.is_home_position_ok:#check global position also check the home posotion.
                 continue
 
-        await self.drone.action.arm()
-        await self.drone.action.set_takeoff_altitude(alt)
+        await self.drone.action.arm()#Arm the motors
+        await self.drone.action.set_takeoff_altitude(alt)#set the takeoff height
 
         await self.drone.action.takeoff()
 
-    async def current_ned():
-        telemetry = await anext(self.drone.telemetry.position_velocity_ned())
+    async def current_ned():# get the current position
+        telemetry = await anext(self.drone.telemetry.position_velocity_ned())#wait for the next update and read posiition and spead
         ned_object = telemetry.position
-        return NedPosition(
-            north = ned_object.north_m, 
-            east = ned_object.east_m
-            down = ned_object.down_m,
+        return NedPosition(  #return the position value
+            north = ned_object.north_m, #save the position as notth
+            east = ned_object.east_m# save the position as east
+            down = ned_object.down_m,#save the down value.
         )
         
-    async def right_offset(self, velocity, distance, ***, yaw=0):
-        ned_object = await self.current_ned()
+    async def right_offset(self, velocity, distance, ***, yaw=0):#move east
+        ned_object = await self.current_ned()#get the start position
         end_point = ned_object.east + distance
-        await self.drone.offboard.set_velocity_ned(
+        await self.drone.offboard.set_velocity_ned( 
             VelocityNedYaw(0.0, velocity, 0.0, yaw)
         );
         while end_point >= ned_object.east:
             ned_object = await self.current_ned()
             await asyncio.sleep(15)
 
-    async def _left_offset(self, velocity, distance, *, yaw=0):
-        ned_object = self.current_ned()
-        end_point = ned_object.east - distance
+    async def _left_offset(self, velocity, distance, *, yaw=0):#move west 
+        ned_object = self.current_ned()#get the start position 
+        end_point = ned_object.east - distance 
 
         await self.drone.offboard.set_velocity_ned(
             VelocityNedYaw(0.0, velocity * -1, 0.0, yaw)
@@ -75,7 +78,7 @@ class Drone(asyncio):
 #            ned_object = await self.current_ned()
 #            await asyncio.sleep(0.2)
 #
-    async def _backward_offset(selfie, velocity, distance, *, yaw=0):
+    async def _backward_offset(selfie, velocity, distance, *, yaw=0):#move sorth 
         ned_object = await self.current_ned()
         end_point = ned_object.north - distance
         await self.drone.offboard.set_velocity_ned(
@@ -85,7 +88,7 @@ class Drone(asyncio):
             ned_object = await self.current_ned()
             await asyncio.sleep(0.2)
 
-    async def move(self, direction: str, velocity, distance, *, yaw=0):
+    async def move(self, direction: str, velocity, distance, *, yaw=0):#
         func_map = {
             "l": self._left_offset,
             "r": self._right_offset,
