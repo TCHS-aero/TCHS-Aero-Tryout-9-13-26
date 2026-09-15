eafe405ec1a14b24cc0990d5818d42739ec8d75b
priscillapriscilla5020-pixel <priscillapriscilla5020@gmail.com>
2026-09-15 01:56:58 +0000

    drone including the takeoff and landing

--- Custom.py
+++ Custom.py
@@ -6,12 +6,42 @@
 # Step 2: Plan
 # Step 3: Code
 # Step 4: Debug
+- Takeoff into the air
+- Wait 10 seconds
+- Fly forwards for 10 meters at 5 m/s
+- Return to launch (including landing)
 
-import asyncio
-from mavsdk import System
+#Have the drone connect to the computer and then takeoff, 
+class Drone(asyncio): 
+    def __init__(self, port = "udpin://0.0.0.0:14540"):
+        self.drone = System()
+        self.port = "udpin://0.0.0.0:14540"
 
-async def main():
-    drone = System("udpin://0.0.0.0:14540")
+    async def connect(self): 
+        connected = False
 
-if __name__ == "__main__":
-    asyncio.run(main)
+        await self.drone.connect(system_address=self.port)
+        async for state in self.drone.core.connection_state():
+            if state.is_connected:
+                break
+await asyncio.sleep(10)
+def takeoff(self, alt) 
+        await self.drone.action.arm()
+        await self.drone.action.set_takeoff_altitude(alt 10 meters at 5 meters/seconds)
+altitude = 0
+while set_takeoff_altitude > 0:
+    print(starting flight)
+    count += 1
+Altitude meters = [2 meters/seconds]
+leave launchpad
+        await self.drone.action.takeoff()
+async def land(self):
+        try?:
+            await self.drone.offboard.stop()
+        except Exception: 
+            pass
+
+        await self.drone.action.land()
+return to launchpad
+
+#Priscilla To priscillapriscilla5020@gmail.com
